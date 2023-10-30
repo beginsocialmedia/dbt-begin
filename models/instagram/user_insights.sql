58b7d14e46a9acef381bda60d02ed1d8c79fd2aa
Matheus Tenório <matheus@beginsocialmedia.com>
2023-10-30 01:12:15 +0000

    Changes to fix errors caused by migrating code from dbt core to dbt cloud

--- models/instagram/user_insights.sql
+++ models/instagram/user_insights.sql
@@ -1,14 +1,17 @@
-WITH user_insights AS (
-    SELECT u.username,
-           COALESCE(ui.reach, 0) AS reach,
-           COALESCE(ui.impressions, 0) AS impressions,
-           COALESCE(ui.website_clicks, 0) AS website_clicks,
-           COALESCE(ui.profile_views, 0) AS profile_views,
-           COALESCE(ui. follower_count) AS follower_count,
-           "date"
-    FROM `begin-data.instagram_raw.users` AS u
-    LEFT JOIN `begin-data.instagram_raw.user_insights` AS ui
-    ON u.id = ui.business_account_id
-)
-SELECT *
-FROM user_insights
+with
+    user_insights as (
+        select
+            u.username,
+            coalesce(ui.reach, 0) as reach,
+            coalesce(ui.impressions, 0) as impressions,
+            coalesce(ui.website_clicks, 0) as website_clicks,
+            coalesce(ui.profile_views, 0) as profile_views,
+            coalesce(ui.follower_count) as follower_count,
+            date
+        from `begin-data.instagram_raw.users` as u
+        left join
+            `begin-data.instagram_raw.user_insights` as ui
+            on u.id = ui.business_account_id
+    )
+select *
+from user_insights
--- models/instagram/users.sql
+++ models/instagram/users.sql
@@ -1,12 +1,14 @@
-WITH user_data AS (
-    SELECT u.username,
-           u.followers_count,
-           u.media_count,
-           u.profile_picture_url,
-           u.name,
-           u.biography,
-           u.id
-    FROM `begin-data.instagram_raw.users` AS u
-)
-SELECT *
-FROM user_data
+with
+    user_data as (
+        select
+            u.username,
+            u.followers_count,
+            u.media_count,
+            u.profile_picture_url,
+            u.name,
+            u.biography,
+            u.id
+        from `begin-data.instagram_raw.users` as u
+    )
+select *
+from user_data
